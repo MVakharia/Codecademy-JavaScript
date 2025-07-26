b866b30e58ac133fc55dd0c14750cc202f144837
MVakharia <mvakharia.bcm@gmail.com>
2025-07-26 13:46:48 +0100

    Update inheritance.js

--- Classes/inheritance.js
+++ Classes/inheritance.js
@@ -44,6 +44,30 @@ class Animal {
     }
 }
 
+class Catatac extends Animal {
+    constructor(name, usesLitter) {
+        super(name);
+
+        this._usesLitter = usesLitter;
+    }
+
+    get usesLitter () {
+        return this._usesLitter;
+    }
+}
+
+const bryceCat = new Catatac('Bryce', false);
+
+console.log(bryceCat._name);
+
+class Dodog extends Animal {
+    constructor(name) {
+        super(name);
+    }
+}
+
+
+
 class HospitalEmployee {
     constructor(name) {
         this._name = name;
@@ -62,4 +86,86 @@ class HospitalEmployee {
     takeVacationDays (daysOff) {
         this._remainingVacationDays -= daysOff;
     }
+
+    static generatePassword () {
+        return Math.floor(Math.random() * 10000);
+    }
 }
+
+class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+        super(name, certifications);
+
+        this._certifications = certifications;
+
+    }
+
+    get certifications () {
+        return this._certifications;
+    }
+
+    addCertification(newCertification) {
+        this._certifications.push(newCertification);
+    }
+}
+
+class Doctor extends HospitalEmployee {
+    constructor(name, remainingVacationDays, insurance) {
+        super(name, remainingVacationDays, insurance);
+
+        this._name = name;
+
+        this._remainingVacationDays = remainingVacationDays;
+
+        this._insurance = insurance;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get remainingVacationDays () {
+        return this._remainingVacationDays;
+    }
+
+    get insurance () {
+        return this._insurance;
+    }
+
+    takeVacationDays(daysOff) {
+        super.takeVacationDays(daysOff);
+    }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+
+nurseOlynyk.takeVacationDays(5);
+
+console.log(nurseOlynyk.remainingVacationDays);
+
+nurseOlynyk.addCertification('Genetics');
+
+console.log(nurseOlynyk.certifications);
+
+class vampireSlayer {
+    constructor(name) {
+        this._name = name;
+
+        this.behaviour = 0;
+    }
+
+    static generateName () {
+        const names=['Angel', 'Spike', 'Buffy', 'Willow', 'Tara'];
+
+        const randomNumber = Math.floor(Math.random()*5);
+
+        return names[randomNumber];
+    }
+}
+
+console.log(vampireSlayer.generateName());
+
+const sally = new vampireSlayer('Sally');
+
+//sally.generateName(); - this generates a type error.
+
