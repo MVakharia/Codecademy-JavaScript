a67df5d36eb0c45efa16e059514530936a66ffc2
MVakharia <mvakharia.bcm@gmail.com>
2025-07-30 19:02:47 +0100

    Update rooster_test.js

--- Mocha/test/rooster_test.js
+++ Mocha/test/rooster_test.js
@@ -8,18 +8,39 @@ describe('Cock-a-doodle-does.', () => {
 
         it('Returns a rooster call.', () => {
 
-            //Setup
+            //SETUP: This is the EXPECTED output
 
             let expected = 'Cock-a-doodle-doo!';
 
-            //Exercise
+            //EXERCISE: Calls Rooster.announceDawn, stores the result in a variable. This is the ACTUAL output.
 
             let roosterCall = Rooster.announceDawn();
 
-            //Verify
+            //VERIFY: Uses an 'assert' method to compare ACTUAL and EXPECTED output.
+
+            assert.equal(roosterCall, expected);
 
 
 
-        })
-    })
+        });
+    });
+
+    describe ('', () => {
+        it('Returns its argument as a string.', () => {
+            //SETUP: This is the EXPECTED output.
+
+            const input = 12;
+            const expected = '12';
+
+            //EXERCISE: Gets the actual output and stores the result in a variable.
+
+            const actual = Rooster.timeAtDawn(input);
+
+            //VERIFY: Uses an 'assert' method to compare ACTUAL and EXPECTED output.
+
+            assert.equal(actual, expected);
+        });
+    });
+
+
 });
